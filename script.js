d6dd962be7063d520e03dc085c078417a84311b9
Ziggyologist <teodora11vela@gmail.com>
2022-08-27 16:50:21 +0300

    fixed height bug

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const projects = [
     id: "cat_finder",
     project_name: "Cat Finder",
     project_description:
-      "LoCAT is a web application designed to help users find and report missing cats. The app makes use of the Geolocation API, as the users can pin on the map the precise location and time where they spotted a cat. Additionally, the color, race, and other details can be provided about the cat via a form. The missing cat profile is then updated to the database, and users can search the database for a specific cat (by color, race etc). \n This app was developped using HTML, CSS, JavaScript, and the Leaflet library. The icons displayed on the map are created by me in Adobe Illustrator.",
+      "LoCAT is a web application designed to help users find and report missing cats. The app makes use of the Geolocation API, as users can pin on the map the precise location and time where they spotted a cat. Additionally, the color, race, and other details can be provided about the cat via a form. The missing cat profile is then updated to the database, and users can search the database for a specific cat (by color, race etc). \n This app was developped using HTML, CSS, JavaScript, and the Leaflet library. The icons displayed on the map are created by me in Adobe Illustrator.",
     project_photo: "",
     project_url: "https://ziggyologist.github.io/Cat-Finder/",
   },
