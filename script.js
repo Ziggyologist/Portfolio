9f12a58776c47ec0d5ede9ae955acd4d715f54c1
Ziggyologist <teodora11vela@gmail.com>
2022-08-28 10:08:03 +0300

    changed gif names

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const projects = [
     project_name: "Cat Finder",
     project_description:
       "LoCAT is a web application designed to help users find and report missing cats. The app makes use of the Geolocation API, as users can pin on the map the precise location and time where they spotted a cat. Additionally, the color, breed, and other details can be provided about the cat via a form. The missing cat profile is then updated to the database, and users can search the database for a specific cat (by color, race etc). \n This app was developped using HTML, CSS, JavaScript, and the Leaflet library. The icons displayed on the map are created by me in Adobe Illustrator. This app was made using the Object Oriented Programming paradigm, with ES6 classes.",
-    project_photo: "./catAp.gif",
+    project_photo: "./CatAp.gif",
     photo_description: "screenshot of cat finder application",
     project_url: "https://ziggyologist.github.io/Cat-Finder/",
   },
@@ -13,7 +13,7 @@ const projects = [
     project_name: "Bullet Journal",
     project_description:
       "The bullet journal app is very similar to the classical paper journal. The users can rate their daily mood by coloring a mountain, insert habits to track, and add diary entries. This app was created using HTML, CSS, and JavaScript, and all the illustrations and SVGs were done in Adobe Illustrator and are originals. ",
-    project_photo: "./bulletAp.GIF",
+    project_photo: "./BulletAp.GIF",
     photo_description: "screenshot of bullet journal application",
     project_url: "https://ziggyologist.github.io/MoodTrackerApp/main_page.html",
   },
@@ -31,7 +31,7 @@ const projects = [
     project_name: "Banking App",
     project_description:
       "This is a banking app simulation. For this project there were created 4 users, each with their own account and history. The 4 accounts can transfer money between each other. In addition, other functions available in this app are sorting the transactions, converting currencies, or making payments. The application also has a timer and the user is logged out after 5 minutes of inactivity. ",
-    project_photo: "./bankAp.gif",
+    project_photo: "./BankAp.gif",
     photo_description: "screenshot of banking application",
     project_url: "https://ziggyologist.github.io/BankApp/",
   },
